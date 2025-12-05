fb1dffe7a61e6985a3e848dfc2a60420086bb78b
Behruz Jumanov <behruzjumanov55@gmail.com>
2025-12-05 16:54:21 +0500

    adde d new changes

---
+++ index.js
@@ -0,0 +1,117 @@
+//1-example
+
+// let age = +prompt("Yoshigizni kiriting:");
+
+// if(age < 18 && age > 0) {
+//   alert("Sizga mumkin emas") ;
+// }else if (age <= 0 || age > 120)  {
+//   alert("Yoshingizni noto'g'ri kiritdingiz")
+// } else
+//   alert("xush kelibsiz")
+
+//2-example
+
+// let age = prompt("age");
+
+// switch (Number(age)) {
+//   case 2:
+//     console.log("Dushanba");
+//     console.log("Dushanba");
+//     break;       // BU YERDA break bo‘lishi shart
+
+//   case 3:
+//     console.log("Chorshanba");
+//     console.log("Chorshanba");
+//     break;
+
+//   case 4:
+//     console.log("Payshanba");
+//     console.log("Payshanba");
+//     break;
+
+//   default:
+//     console.log("Bunday kun yo‘q");
+// }
+
+//3
+// let year = +prompt("biror yilni kiriting");
+// let num = 30;
+
+// for (let i = 1; (num) => 0; i++) {
+//   if (num % 2 == 0) {
+//     console.log();
+//   }
+// }
+
+//new example👇 case
+//1
+// let num = +prompt("sonni kiriting")
+// switch (num) {
+//     case 1:
+//         console.log("Dushanba");
+//         console.log("Dushanba");
+//                 break;
+//         case 2:
+//             console.log("Seshanba");
+//             console.log("Seshanba");
+//                     break;
+//             case 3:
+//                 console.log("Chorshanba");
+//                 console.log("Chorshanba");
+//                         break;
+//                 case 4:
+//                 console.log("Chorshanba");
+//                 console.log("Chorshanba");
+//                         break;
+
+//                 case 5:
+//                 console.log("Chorshanba");
+//                 console.log("Chorshanba");
+//                         break;
+
+//                 case 6:
+//                 console.log("Chorshanba");
+//                 console.log("Chorshanba");
+//                         break;
+//                 case 7:
+//                 console.log("Chorshanba");
+//                 console.log("Chorshanba");
+//                         break;
+//         break;
+
+//     default:
+// console.log("hech narsa yo'q");
+// }
+//2
+let price = +prompt("baho kiriting");
+switch (price) {
+  case 1:
+    console.log("yomon");
+    console.log("yomon");
+    break;
+  case 2:
+    console.log("qoniqarsiz");
+    console.log("qoniqarsiz");
+    break;
+  case 3:
+    console.log("qoniqarli");
+    console.log("qoniqarli");
+    break;
+
+  case 4:
+    console.log("yaxshi");
+    console.log("yaxshi");
+    break;
+  case 5:
+    console.log("a'lo");
+    console.log("a'lo");
+    break;
+  case 5:
+    console.log("great");
+    console.log("great");
+    break;
+  default:
+    console.log("bunaqa haho yo'q");
+
+    break;
+}
